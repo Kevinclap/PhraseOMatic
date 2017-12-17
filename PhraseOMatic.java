610ce44bb1f81a5c472abd85070524adf16b674a
Kevin Castro Cruz <kevinclap@Kevins-MacBook-Pro.local>
2017-12-17 05:06:24 -0500

    First commit

---
+++ PhraseOMatic.java
@@ -0,0 +1,25 @@
+
+public class PhraseOMatic {
+
+	public static void main(String[] args) {
+		String[] wordListOne = {"24/7", "Multi-Tier", "30,00 feet", "B-to-B", "win-win"};
+		
+		String[] wordListTwo = {"empowered", "oriented", "centric", "branded","focused", "outside the box"};
+		
+		String[] wordListThree = {"process", "solution", "architecture", "core competency", "mission"};
+		
+		int oneLength = wordListOne.length;
+		int twoLength = wordListTwo.length;
+		int threeLength = wordListThree.length;
+		
+		int rand1 = (int) (Math.random() * oneLength);
+		int rand2 = (int) (Math.random() * twoLength);
+		int rand3 = (int) (Math.random() * threeLength);
+		
+		String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+		
+		System.out.println("What we need is a " + phrase + ".");
+	}
+}
+
+
